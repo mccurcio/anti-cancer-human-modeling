1ab0587eb85f45cbebd88d12cab392a16996c241
mccurcio <matt.curcio.ri@gmail.com>
2019-09-02 12:50:01 -0400

    Moved Anti-Caner & Human data to 1data

---
+++ factominer-NO-GOOD/code.R
@@ -0,0 +1,182 @@
+library(FactoMineR)
+drawn <-
+integer(0)
+plot.PCA(memory, choix = 'ind', invisible = c('var', 'quali'), select = drawn, title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, choix = 'ind', invisible = c('var', 'quali'), select = drawn, title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(memory, choix = 'var', select = drawn, title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, choix = 'var', select = drawn, title = '', cex = cex)
+barplot(res$eig[,2], names.arg = 1:nrow(res$eig))
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 1:2, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 1:2, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 3:4, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 3:4, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 5:6, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 5:6, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 7:8, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 7:8, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 9:10, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 9:10, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 11:12, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 11:12, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 13:14, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 13:14, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 15:16, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 15:16, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 17:18, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 17:18, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 19:20, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 19:20, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 21:22, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 21:22, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 23:24, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 23:24, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 25:26, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 25:26, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 27:28, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 27:28, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 29:30, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 29:30, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 31:32, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 31:32, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 33:34, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 33:34, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 35:36, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 35:36, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 37:38, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 37:38, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 39:40, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 39:40, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 41:42, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 41:42, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 43:44, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 43:44, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 45:46, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 45:46, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 47:48, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 47:48, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 49:50, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 49:50, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 51:52, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 51:52, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 53:54, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 53:54, choix = 'var', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 55:56, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+drawn <-
+integer(0)
+plot.PCA(res, select = drawn, axes = 55:56, choix = 'var', title = '', cex = cex)
